507ca6469db7ae618ac87d35ecd4a4247118d594
Seth Stadick <sstadick@gmail.com>
2020-01-23 14:44:16 -0800

    Working with both Lapper and ScAIList

--- src/lib.rs
+++ src/lib.rs
@@ -1,71 +1,18 @@
-use ivtools::{Bits, Interval, IvStore, Lapper, ScAIList};
 use pyo3::prelude::*;
-use pyo3::types::{PyAny, PyList};
 // use pyo3::wrap_pyfunction;
+mod pyrinterval;
+mod pyrlapper;
+mod pyrscailist;
 
-#[pyclass]
-struct PyrInterval {
-    #[pyo3(get, set)]
-    start: u32,
-    #[pyo3(get, set)]
-    stop: u32,
-    #[pyo3(get, set)]
-    val: String,
-}
-
-#[pymethods]
-impl PyrInterval {
-    #[new]
-    fn new(start: u32, stop: u32, val: String) -> Self {
-        PyrInterval {
-            start: start,
-            stop: stop,
-            val: val,
-        }
-    }
-}
-
-#[pyclass]
-struct PyrLapper {
-    lapper: Lapper<String>,
-}
-
-#[pymethods]
-impl PyrLapper {
-    #[new]
-    fn new(ivs: Vec<&PyAny>) -> Self {
-        PyrLapper {
-            lapper: Lapper::new(
-                ivs.iter()
-                    .flat_map(|iv| {
-                        if let Ok(iv) = iv.cast_as::<PyrInterval>() {
-                            Some(Interval {
-                                start: iv.start,
-                                stop: iv.stop,
-                                val: iv.val.to_string(),
-                            })
-                        } else {
-                            None
-                        }
-                    })
-                    .collect(),
-            ),
-        }
-    }
-
-    fn find(&self, py: Python<'_>, start: u32, stop: u32) -> PyResult<Vec<PyrInterval>> {
-        let mut result = vec![];
-        for iv in self.lapper.find(start, stop) {
-            result.push(PyrInterval::new(iv.start, iv.stop, iv.val.clone()))
-        }
-        Ok(result)
-    }
-}
+pub use self::pyrinterval::PyrInterval;
+pub use self::pyrlapper::PyrLapper;
+pub use self::pyrscailist::PyrScailist;
 
 /// This module is a python module implemented in Rust.PartialEq
 #[pymodule]
 fn pyr_lapper(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_class::<PyrLapper>()?;
     m.add_class::<PyrInterval>()?;
+    m.add_class::<PyrScailist>()?;
     Ok(())
 }
